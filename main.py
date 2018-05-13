c7dc02eac226a54345ced85edd03c081d9d1ff49
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-05-13 19:18:46 +0430

    save all file pathes

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+from PIL import Image
+import matplotlib.image as image
+import numpy as np
+from os import listdir
+
+ROOT = "notMNIST_large"
+DS_STORE = ".DS_Store"
+
+
+def readImageMatrix(path):
+    img = image.imread(path)
+    return np.asmatrix(img)
+
+
+def createDataSet():
+    folders = listdir(ROOT)
+    if DS_STORE in folders:
+        folders.remove(DS_STORE)
+
+    dataSet = []
+    for folder in folders:
+        files = listdir("%s/%s" % (ROOT, folder))
+        if DS_STORE in files:
+            files.remove(DS_STORE)
+
+        dataSet += [("%s/%s/%s" % (ROOT, folder, file)) for file in files]
+
+    return dataSet
 
 
 if __name__ == "__main__":
-    print("START!");
+    ds = createDataSet()
